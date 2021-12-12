0227c27ffe1b6ce7cfaab39f11d734d9c3647121
rodolfosrd <rodolfo.quispe@upeu.edu.pe>
2021-12-12 14:36:22 -0500

    Page No Found Styles

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,9 +2,29 @@ import React from 'react'
 
 function Home() {
     return (
-        <div>
-            asdsad
-        </div>
+    
+
+      <div>
+     <h1>
+      PONGA EN PRACTICA ESTOS <br />
+   CONSEJOS EN SU VIDA Y EVITE LA
+   <br />
+ MAYORIA DE ENFERMEDADES
+            </h1>
+            
+     <p>
+  Tiene en sus manos los 8
+ remedios naturales
+    
+ que Dios nos dejo para poder
+        
+    mantenernos con buena salud o
+ para restablecer la salud
+                </p>
+            
+              
+            </div>
+       
     )
 }
 
--- src/pages/PageNotFount.js
+++ src/pages/PageNotFount.js
@@ -1,10 +1,18 @@
 import React from 'react'
+import  NE from "../assets/NE.jpg";
 
 function PageNotFount() {
     return (
         <div>
-            <h1>aea</h1>
-        </div>
+            <h1>Pagina </h1>
+       
+         
+            
+        
+         <div class="image">
+         <img src={NE} />
+       </div>
+       </div>
     )
 }
 
