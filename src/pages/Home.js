261c503b4c37f1eed59c3faacbf5a159491475ce
Rodolfo Quispe Conza <50849056+rodolfosrd@users.noreply.github.com>
2021-12-14 22:45:50 -0500

    styles

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,7 +8,7 @@ function Home() {
       <div class="contenedor">
         <div class="contenedordos">
           <center>
-            <h1 class="texto">8 REMEDIOS NATURALES</h1>
+            <h1 class="texto">GUÍA PRÁCTICA ADVENTISTA DE LOS OCHO REMEDIOS</h1>
           </center>
 
           <div class="seccion">
@@ -51,7 +51,7 @@ function Home() {
             </div>
           </div>
           <center>
-            <h1 class="texto">8 REMEDIOS NATURALES</h1>
+            <h1 class="texto">LA ALIMENTACION</h1>
           </center>
 
           <div class="seccion">
@@ -93,6 +93,175 @@ function Home() {
               <img src={RO} />
             </div>
           </div>
+          <center>
+            <h1 class="texto">LA ALIMENTACION</h1>
+          </center>
+
+          <div class="seccion">
+            <div class="parrafo">
+              <br></br>
+              <b>
+                <p>
+                  Son aquellas técnicas utilizadas desde el hogar a partir de
+                  las plantas
+                  <br />
+                  naturales que nos brinda el medio ambiente y que cuentan con{" "}
+                  <br />
+                  propiedades muy efectivas para hacer frente a problemas de
+                  salud <br />
+                  en el cuerpo humano.
+                  <br />
+                  <br></br>
+                  “Así que, hermanos, os ruego por las misericordias de Dios,
+                  que
+                  <br />
+                  presentéis vuestros cuerpos en sacrificio vivo, santo,
+                  agradable a Dios,
+                  <br />
+                  que es vuestro culto racional. No os conforméis a este siglo,
+                  sino
+                  <br />
+                  transformaos
+                  <br />
+                  por medio de la renovación de vuestro entendimiento, para que
+                  comprobéis <br />
+                  cuál sea <br />
+                  la buena voluntad de Dios, agradable y perfecta”.
+                  <br />
+                  Romanos 12:1, 2<br />
+                </p>
+              </b>
+            </div>
+            <div class="image">
+              <img src={RO} />
+            </div>
+          </div>
+          <center>
+            <h1 class="texto">LA ALIMENTACION</h1>
+          </center>
+
+          <div class="seccion">
+            <div class="parrafo">
+              <br></br>
+              <b>
+                <p>
+                  Son aquellas técnicas utilizadas desde el hogar a partir de
+                  las plantas
+                  <br />
+                  naturales que nos brinda el medio ambiente y que cuentan con{" "}
+                  <br />
+                  propiedades muy efectivas para hacer frente a problemas de
+                  salud <br />
+                  en el cuerpo humano.
+                  <br />
+                  <br></br>
+                  “Así que, hermanos, os ruego por las misericordias de Dios,
+                  que
+                  <br />
+                  presentéis vuestros cuerpos en sacrificio vivo, santo,
+                  agradable a Dios,
+                  <br />
+                  que es vuestro culto racional. No os conforméis a este siglo,
+                  sino
+                  <br />
+                  transformaos
+                  <br />
+                  por medio de la renovación de vuestro entendimiento, para que
+                  comprobéis <br />
+                  cuál sea <br />
+                  la buena voluntad de Dios, agradable y perfecta”.
+                  <br />
+                  Romanos 12:1, 2<br />
+                </p>
+              </b>
+            </div>
+            <div class="image">
+              <img src={RO} />
+            </div>
+          </div>
+          <center>
+            <h1 class="texto">LA ALIMENTACION</h1>
+          </center>
+          <div class="seccion">
+            <div class="parrafo">
+              <br></br>
+              <b>
+                <p>
+                  Son aquellas técnicas utilizadas desde el hogar a partir de
+                  las plantas
+                  <br />
+                  naturales que nos brinda el medio ambiente y que cuentan con{" "}
+                  <br />
+                  propiedades muy efectivas para hacer frente a problemas de
+                  salud <br />
+                  en el cuerpo humano.
+                  <br />
+                  <br></br>
+                  “Así que, hermanos, os ruego por las misericordias de Dios,
+                  que
+                  <br />
+                  presentéis vuestros cuerpos en sacrificio vivo, santo,
+                  agradable a Dios,
+                  <br />
+                  que es vuestro culto racional. No os conforméis a este siglo,
+                  sino
+                  <br />
+                  transformaos
+                  <br />
+                  por medio de la renovación de vuestro entendimiento, para que
+                  comprobéis <br />
+                  cuál sea <br />
+                  la buena voluntad de Dios, agradable y perfecta”.
+                  <br />
+                  Romanos 12:1, 2<br />
+                </p>
+              </b>
+            </div>
+            <div class="image">
+              <img src={RO} />
+            </div>
+          </div>
+
+          <div class="seccion">
+            <div class="parrafo">
+              <br></br>
+              <b>
+                <p>
+                  Son aquellas técnicas utilizadas desde el hogar a partir de
+                  las plantas
+                  <br />
+                  naturales que nos brinda el medio ambiente y que cuentan con{" "}
+                  <br />
+                  propiedades muy efectivas para hacer frente a problemas de
+                  salud <br />
+                  en el cuerpo humano.
+                  <br />
+                  <br></br>
+                  “Así que, hermanos, os ruego por las misericordias de Dios,
+                  que
+                  <br />
+                  presentéis vuestros cuerpos en sacrificio vivo, santo,
+                  agradable a Dios,
+                  <br />
+                  que es vuestro culto racional. No os conforméis a este siglo,
+                  sino
+                  <br />
+                  transformaos
+                  <br />
+                  por medio de la renovación de vuestro entendimiento, para que
+                  comprobéis <br />
+                  cuál sea <br />
+                  la buena voluntad de Dios, agradable y perfecta”.
+                  <br />
+                  Romanos 12:1, 2<br />
+                </p>
+              </b>
+            </div>
+            <div class="image">
+              <img src={RO} />
+            </div>
+          </div>
+
         </div>
       </div>
     </div>
