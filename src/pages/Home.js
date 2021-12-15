8e873084c988424770d1ab86f671ccc90d1b1219
Rodolfo Quispe Conza <50849056+rodolfosrd@users.noreply.github.com>
2021-12-15 01:29:08 -0500

    styles home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,8 @@
 import React from "react";
 import "../estilos/Home.css";
 import RO from "../assets/RO.jpg";
-
+import KK from "../assets/KK.jpg";
+import AG from "../assets/AG.png";
 function Home() {
   return (
     <div class="home">
@@ -14,132 +15,74 @@ function Home() {
           <div class="seccion">
             <div class="parrafo">
               <br></br>
-              <b>
-                <p>
-                  Son aquellas técnicas utilizadas desde el hogar a partir de
-                  las plantas
-                  <br />
-                  naturales que nos brinda el medio ambiente y que cuentan con{" "}
-                  <br />
-                  propiedades muy efectivas para hacer frente a problemas de
-                  salud <br />
-                  en el cuerpo humano.
-                  <br />
-                  <br></br>
-                  “Así que, hermanos, os ruego por las misericordias de Dios,
-                  que
-                  <br />
-                  presentéis vuestros cuerpos en sacrificio vivo, santo,
-                  agradable a Dios,
-                  <br />
-                  que es vuestro culto racional. No os conforméis a este siglo,
-                  sino
-                  <br />
-                  transformaos
-                  <br />
-                  por medio de la renovación de vuestro entendimiento, para que
-                  comprobéis <br />
-                  cuál sea <br />
-                  la buena voluntad de Dios, agradable y perfecta”.
-                  <br />
-                  Romanos 12:1, 2<br />
-                </p>
-              </b>
-            </div>
-            <div class="image">
-              <img src={RO} />
-            </div>
-          </div>
-          <center>
-            <h1 class="texto">LA ALIMENTACION</h1>
-          </center>
+              
+            <b> <p class = "ioio">
+            Los remedios naturales son gratuitos y efectivos para la salud y el cuidado de nuestro
+             cuerpo, actualmente disfrutar de buena salud resulta un  tanto difícil debido a la 
+             rutina que muchas veces llevamos pero no es imposible, es cuestión de tomar acción y 
+             decidir por lo que realmente nos favorece. Además estos remedios son una bendición que 
+             nuestro Padre creador a puesto a disposición de todos, entonces ¿Por qué no empezar a
+              usarlos?  Estos remedios son: Agua – Descanso – Ejercicios – Luz Solar – Aire Puro 
+              – Nutrición – Temperancia – Esperanza.
+                 
 
-          <div class="seccion">
-            <div class="parrafo">
-              <br></br>
-              <b>
-                <p>
-                  Son aquellas técnicas utilizadas desde el hogar a partir de
-                  las plantas
-                  <br />
-                  naturales que nos brinda el medio ambiente y que cuentan con{" "}
-                  <br />
-                  propiedades muy efectivas para hacer frente a problemas de
-                  salud <br />
-                  en el cuerpo humano.
-                  <br />
-                  <br></br>
-                  “Así que, hermanos, os ruego por las misericordias de Dios,
-                  que
-                  <br />
-                  presentéis vuestros cuerpos en sacrificio vivo, santo,
-                  agradable a Dios,
-                  <br />
-                  que es vuestro culto racional. No os conforméis a este siglo,
-                  sino
-                  <br />
-                  transformaos
-                  <br />
-                  por medio de la renovación de vuestro entendimiento, para que
-                  comprobéis <br />
-                  cuál sea <br />
-                  la buena voluntad de Dios, agradable y perfecta”.
-                  <br />
-                  Romanos 12:1, 2<br />
-                </p>
-              </b>
-            </div>
-            <div class="image">
-              <img src={RO} />
-            </div>
-          </div>
-          <center>
-            <h1 class="texto">LA ALIMENTACION</h1>
-          </center>
 
-          <div class="seccion">
-            <div class="parrafo">
-              <br></br>
-              <b>
-                <p>
-                  Son aquellas técnicas utilizadas desde el hogar a partir de
-                  las plantas
-                  <br />
-                  naturales que nos brinda el medio ambiente y que cuentan con{" "}
-                  <br />
-                  propiedades muy efectivas para hacer frente a problemas de
-                  salud <br />
-                  en el cuerpo humano.
-                  <br />
-                  <br></br>
-                  “Así que, hermanos, os ruego por las misericordias de Dios,
-                  que
-                  <br />
-                  presentéis vuestros cuerpos en sacrificio vivo, santo,
-                  agradable a Dios,
-                  <br />
-                  que es vuestro culto racional. No os conforméis a este siglo,
-                  sino
-                  <br />
-                  transformaos
-                  <br />
-                  por medio de la renovación de vuestro entendimiento, para que
-                  comprobéis <br />
-                  cuál sea <br />
-                  la buena voluntad de Dios, agradable y perfecta”.
-                  <br />
-                  Romanos 12:1, 2<br />
-                </p>
-              </b>
+                </p></b> 
+            
             </div>
             <div class="image">
               <img src={RO} />
             </div>
           </div>
-          <center>
-            <h1 class="texto">LA ALIMENTACION</h1>
-          </center>
+         
+            <center><h1 class="texto">AIRE PURO</h1></center>
+        
+        <div class="seccion">
+            <div class="parrafo">
+              <br></br>
+              <b>
+                <p>
+                El oxígeno es esencial para la vida. Respire aire puro para mantener una buena salud.<br />
+                 Cada célula necesita de oxígeno para hacer funcionar sus “usinas” de energía.<br />
+                 <br></br>
+                  Los glóbulos rojos de la sangre, por lo tanto, transportan el dióxido de carbono<br />
+                   de vuelta a los pulmones. Cuando expiramos, el gas carbónico es expulsado con el<br />
+                    aire pobre en oxígeno.  Es fundamental buscar lugares donde haya ventilación y<br />
+                     que el aire a ser respirado sea puro.
+                  
+                   que el aire a ser respirado sea puro.<br />
 
+                </p>
+              </b>
+            </div>
+            <div class="image">
+              <img src={KK} />
+            </div>
+          </div>
+           <center><h1 class="texto">EL AGUA</h1></center>
+          <div class="seccion">
+            <div class="parrafo">
+              <br></br>
+              <b>
+                <p>
+              “Estando sanos o enfermos, el agua pura es para nosotros una de las más exquisitas bendiciones<br />
+                 del cielo.<br />
+                Su empleo conveniente favorece la salud. Es la bebida que Dios proveyó para apagar la sed de<br />
+                los animales y del hombre. Ingerida en cantidades suficientes, el agua suple las necesidades<br /> 
+                del organismo, y ayuda a la naturaleza a resistir la enfermedad.” El Ministerio de Curación.<br />
+                <br></br>
+                
+            
+                </p>
+              </b>
+            </div>
+            <div class="image">
+              <img src={AG} />
+            </div>
+          </div>
+        
+            <center><h1 class="texto">LA ALIMENTACION</h1></center>
+       
           <div class="seccion">
             <div class="parrafo">
               <br></br>
@@ -187,33 +130,17 @@ function Home() {
               <br></br>
               <b>
                 <p>
-                  Son aquellas técnicas utilizadas desde el hogar a partir de
-                  las plantas
-                  <br />
-                  naturales que nos brinda el medio ambiente y que cuentan con{" "}
-                  <br />
-                  propiedades muy efectivas para hacer frente a problemas de
-                  salud <br />
-                  en el cuerpo humano.
-                  <br />
-                  <br></br>
-                  “Así que, hermanos, os ruego por las misericordias de Dios,
-                  que
-                  <br />
-                  presentéis vuestros cuerpos en sacrificio vivo, santo,
-                  agradable a Dios,
-                  <br />
-                  que es vuestro culto racional. No os conforméis a este siglo,
-                  sino
-                  <br />
-                  transformaos
-                  <br />
-                  por medio de la renovación de vuestro entendimiento, para que
-                  comprobéis <br />
-                  cuál sea <br />
-                  la buena voluntad de Dios, agradable y perfecta”.
-                  <br />
-                  Romanos 12:1, 2<br />
+                Una alimentación sana involucra dos aspectos: evitar los alimentos <br />
+                que perjudican al organismo y usar con moderación los alimentos que<br />
+                 son beneficiosos, destacando la alimentación vegetariana rica en <br />
+                 fibras  y nutrientes encontrados en los alimentos integrales. <br />
+                 <br></br> 
+                 En cuanto a lp que debe ser evitado están las bebidas estimulantes <br />
+                 como las energizantes, el té, el café, el mate, las drogas lícitas<br />
+                  como el alcohol y el tabaco en todas sus variantes y algunos tipos de<br />
+                   alimentos.
+
+
                 </p>
               </b>
             </div>
